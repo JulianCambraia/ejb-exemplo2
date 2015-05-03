0f5005a61bdf0b25c1e5e0b669ab69a9cef7d999
JulianFernando <juliancambraia@gmail.com>
2015-05-03 20:33:52 -0300

    Cria a Interface de negócios

---
+++ calculadoraremotaClient/ejbModule/br/com/julianfernando/ejb/remote/stateless/RemoteCalculator.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package br.com.julianfernando.ejb.remote.stateless;
+
+/**
+ * @author julian fernando
+ *
+ */
+public interface RemoteCalculator {
+	
+	double soma(double a, double b);
+	double subtracao(double a, double b);
+
+}
