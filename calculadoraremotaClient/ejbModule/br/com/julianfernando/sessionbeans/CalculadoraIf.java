48933306d83ec64ad57b905d9b726f5f7d6ae8a4
JulianFernando <juliancambraia@gmail.com>
2015-05-06 23:39:05 -0300

    Cria a classe deInterface que Implementa os SLSB

---
+++ calculadoraremotaClient/ejbModule/br/com/julianfernando/sessionbeans/CalculadoraIf.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.com.julianfernando.sessionbeans;
+
+/**
+ * Classe que implementa os SLSB devem ser colocadas nesse projeto
+ * @author julian
+ *
+ */
+public interface CalculadoraIf {
+
+	public int soma(int a, int b);
+	
+	public int subtracao(int a, int b);
+	
+	public double multiplicacao(double a, double b);
+	
+	public double divisao(double a, double b);
+}
